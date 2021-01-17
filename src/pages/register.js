fbf03244ae99816eda9c740b38affdef4fd312ac
João Vitor Garcia <joaovitorgarcia@MBP-de-Joao.lan>
2021-01-16 23:08:21 -0300

    init pages repo

--- src/pages/register.js
+++ src/pages/register.js
@@ -1,7 +1,25 @@
+import ponte from '../assets/ponte.png';
+import './register.css';
+
 function Register () {
   return(
     <div>
-      
+      <img src={ponte} alt="ponte"className="imagem"/>
+      <h1 className="cadastro">Cadastro</h1>
+      <div>
+        <label className="nomes">Nome</label>
+        <input placeholder="Nome" required="true"/> 
+      </div>
+      <div>
+        <label>Email</label>
+        <input placeholder="Email"/> 
+      </div>
+      <div>
+        <label>Idade</label>
+        <input placeholder="idade"/> 
+      </div>
     </div>
   )
 }
+
+export default Register
